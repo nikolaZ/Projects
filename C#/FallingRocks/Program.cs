9e7c00b61718780fd363998e99489da654b0a092
Nikola <nikola.zagorchev@gmail.com>
2013-02-17 14:56:10 +0200

    Added C# projects

---
+++ C#/FallingRocks/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+
+struct RocksPattern
+{
+    public static string[] Type = { "^", "@", "*", "&", "+", "%", "$", "#", "!", ".", ";" };
+    public static ConsoleColor[] Colors = { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Cyan,
+                                                ConsoleColor.Green, ConsoleColor.Magenta };
+}
+struct Object
+{
+    public int x;
+    public int y;
+    public ConsoleColor color;
+    public string d;
+}
+struct Rocks
+{
+    public int x;
+    public int y;
+    public ConsoleColor color;
+    public string r;
+}
+
+class fallingRocks
+{
+    static void PrintOnPosition(int x, int y, string c, ConsoleColor color = ConsoleColor.Gray)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.ForegroundColor = color;
+        Console.Write(c);
+    }
+    static void PrintInfo(int x, int y, string c, ConsoleColor color = ConsoleColor.Gray)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.ForegroundColor = color;
+        Console.Write(c);
+    }
+    static void Main()
+    {
+        int playground = 11;
+        byte life = 3;
+
+        Console.BufferHeight = Console.WindowHeight = 20;
+        Console.BufferWidth = Console.WindowWidth = 50;
+        Object dwarf = new Object();
+        dwarf.x = 4;
+        dwarf.y = Console.WindowHeight - 1;
+        dwarf.color = ConsoleColor.White;
+        dwarf.d = "(0)";
+        Random generator = new Random();
+        List<Rocks> rocks = new List<Rocks>();
+
+
+        List<string> typer = new List<string> { "^", "@", "*", "&", "+", "%", "$", "#", "!", ".", ";" };
+        Random typer_rand = new Random();
+        int index = typer_rand.Next(0, 10);
+        string value = typer[index];
+        List<ConsoleColor> color = new List<ConsoleColor> {ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Cyan,
+                                                ConsoleColor.Green, ConsoleColor.Magenta };
+        Random color_rand = new Random();
+        int indexColor = typer_rand.Next(0, 4);
+        ConsoleColor rockColor = color[indexColor];
+
+
+        while (true)
+        {
+            bool hit = false;
+            {
+                Rocks newRock = new Rocks();
+                newRock.color = rockColor;
+                newRock.x = generator.Next(0, playground);
+                newRock.y = 0;
+                newRock.r = value;
+                rocks.Add(newRock);
+            }
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                while (Console.KeyAvailable) Console.ReadKey();//clear other buttons for smooth moving(not neccessary)
+                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                {
+                    if (dwarf.x - 1 >= 0)
+                    {
+                        dwarf.x = dwarf.x - 1;
+                    }
+                }
+                else if (pressedKey.Key == ConsoleKey.RightArrow)
+                {
+                    if (dwarf.x + 1 < playground)
+                    {
+                        dwarf.x = dwarf.x + 1;
+                    }
+                }
+            }
+            List<Rocks> newList = new List<Rocks>();
+            for (int i = 0; i < rocks.Count; i++)
+            {
+                Rocks oldRock = rocks[i];
+                Rocks newRock = new Rocks();
+                newRock.x = oldRock.x;
+                newRock.y = oldRock.y + 1;
+                newRock.r = oldRock.r;
+                newRock.color = oldRock.color;
+                if (newRock.y == dwarf.y && newRock.x == dwarf.x)
+                {
+                    life--;
+                    hit = true;
+                    if (life <= 0)
+                    {
+                        PrintInfo(20, 8, "WASTED", ConsoleColor.DarkRed);
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                if (newRock.y < Console.WindowHeight)
+                {
+                    newList.Add(newRock);
+                }
+            }
+            rocks = newList;
+            Console.Clear();
+            foreach (Rocks rock in rocks)
+            {
+                PrintOnPosition(rock.x, rock.y, rock.r, rock.color);
+            }
+            if (hit)
+            {
+                PrintOnPosition(dwarf.x, dwarf.y, "XXX", ConsoleColor.Red);
+            }
+            else
+            {
+                PrintOnPosition(dwarf.x, dwarf.y, dwarf.d, dwarf.color);
+            }
+            PrintInfo(15, 5, "Lifes: " + life, ConsoleColor.White);
+            PrintInfo(15, 5, "Lifes: " + life, ConsoleColor.White);
+            Thread.Sleep(150);
+        }
+
+    }
+}
+
+
---
+++ C#/NumberText/NumberText.cs
@@ -0,0 +1,179 @@
+﻿//* Write a program that converts a number in the range [0...999] to a text corresponding to its English pronunciation. Examples:
+//	0  "Zero"
+//	273  "Two hundred seventy three"
+//	400  "Four hundred"
+//	501  "Five hundred and one"
+//	711  "Seven hundred and eleven"
+using System;
+using System.Text;
+class NumberText
+{
+    static void Main()
+    {
+        Console.Write("Enter a number in the interval [0...999]: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number > 999)
+        {
+            Console.WriteLine("Entered Number is not in the correct interval!");
+        }
+        else
+        {
+
+
+            int hundreds = number / 100;
+            int tens = (number % 100) / 10;
+            //int units = ((number % 100) % (tens*10));
+            //int realUnits = units % (tens*10);
+            //Console.WriteLine(tens);
+            //Console.WriteLine(units);
+            //Console.WriteLine(realUnits);
+            if (number == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            if (hundreds > 0)
+            {
+                switch (hundreds)
+                {
+                    case 1: Console.Write("one hundred"); break;
+                    case 2: Console.Write("two hundred"); break;
+                    case 3: Console.Write("three hundred"); break;
+                    case 4: Console.Write("four hundred"); break;
+                    case 5: Console.Write("five hundred"); break;
+                    case 6: Console.Write("six hundred"); break;
+                    case 7: Console.Write("seven hundred"); break;
+                    case 8: Console.Write("eight hundred"); break;
+                    case 9: Console.Write("nine hundred"); break;
+                }
+                if (tens > 1)
+                {
+                    switch (tens)//defines tens
+                    {
+                        case 2: Console.Write(" twenty"); break;
+                        case 3: Console.Write(" thirty"); break;
+                        case 4: Console.Write(" fourty"); break;
+                        case 5: Console.Write(" fifty"); break;
+                        case 6: Console.Write(" sixty"); break;
+                        case 7: Console.Write(" seventy"); break;
+                        case 8: Console.Write(" eighty"); break;
+                        case 9: Console.Write(" ninety"); break;
+                    }
+                    int units = ((number % 100) % (tens * 10));//defines units
+                    switch (units)
+                    {
+                        case 1: Console.Write(" one"); break;
+                        case 2: Console.Write(" two"); break;
+                        case 3: Console.Write(" three"); break;
+                        case 4: Console.Write(" four"); break;
+                        case 5: Console.Write(" five"); break;
+                        case 6: Console.Write(" six"); break;
+                        case 7: Console.Write(" seven"); break;
+                        case 8: Console.Write(" eight"); break;
+                        case 9: Console.Write(" nine"); break;
+                    }
+                }
+                if (tens == 0)
+                {
+                    int units = (number % 100);
+                    switch (units)
+                    {
+                        case 1: Console.Write(" and one"); break;
+                        case 2: Console.Write(" and two"); break;
+                        case 3: Console.Write(" and three"); break;
+                        case 4: Console.Write(" and four"); break;
+                        case 5: Console.Write(" and five"); break;
+                        case 6: Console.Write(" and six"); break;
+                        case 7: Console.Write(" and seven"); break;
+                        case 8: Console.Write(" and eight"); break;
+                        case 9: Console.Write(" and nine"); break;
+                    }
+                }
+                if (tens == 1)
+                {
+                    int units1 = (number % 100) % 10;
+                    switch (units1)
+                    {
+                        case 1: Console.Write(" and eleven"); break;
+                        case 2: Console.Write(" and twelve"); break;
+                        case 3: Console.Write(" and thirteen"); break;
+                        case 4: Console.Write(" and fourteen"); break;
+                        case 5: Console.Write(" and fifteen"); break;
+                        case 6: Console.Write(" and sixteen"); break;
+                        case 7: Console.Write(" and seventeen"); break;
+                        case 8: Console.Write(" and eighteen"); break;
+                        case 9: Console.Write(" and nineteen"); break;
+                    }
+                }
+            }
+            else//example 051;(51);
+            {
+                if (tens > 1)//example:51
+                {
+                    switch (tens)//defines tens
+                    {
+                        case 2: Console.Write("twenty"); break;
+                        case 3: Console.Write("thirty"); break;
+                        case 4: Console.Write("fourty"); break;
+                        case 5: Console.Write("fifty"); break;
+                        case 6: Console.Write("sixty"); break;
+                        case 7: Console.Write("seventy"); break;
+                        case 8: Console.Write("eighty"); break;
+                        case 9: Console.Write("ninety"); break;
+                    }
+                    int units = ((number % 100) % (tens * 10));//defines units
+                    switch (units)
+                    {
+                        case 1: Console.Write(" one"); break;
+                        case 2: Console.Write(" two"); break;
+                        case 3: Console.Write(" three"); break;
+                        case 4: Console.Write(" four"); break;
+                        case 5: Console.Write(" five"); break;
+                        case 6: Console.Write(" six"); break;
+                        case 7: Console.Write(" seven"); break;
+                        case 8: Console.Write(" eight"); break;
+                        case 9: Console.Write(" nine"); break;
+                    }
+
+                }
+                else if (tens == 0)//example: 001;(1);
+                {
+                    int units3 = (number % 100);
+                    switch (units3)
+                    {
+                        case 1: Console.Write("one"); break;
+                        case 2: Console.Write("two"); break;
+                        case 3: Console.Write("three"); break;
+                        case 4: Console.Write("four"); break;
+                        case 5: Console.Write("five"); break;
+                        case 6: Console.Write("six"); break;
+                        case 7: Console.Write("seven"); break;
+                        case 8: Console.Write("eight"); break;
+                        case 9: Console.Write("nine"); break;
+                    }
+
+                }
+                if (tens == 1)//011;
+                {
+                    int units4 = (number % 100) % 10;
+                    switch (units4)
+                    {
+                        case 1: Console.Write("eleven"); break;
+                        case 2: Console.Write("twelve"); break;
+                        case 3: Console.Write("thirteen"); break;
+                        case 4: Console.Write("fourteen"); break;
+                        case 5: Console.Write("fifteen"); break;
+                        case 6: Console.Write("sixteen"); break;
+                        case 7: Console.Write("seventeen"); break;
+                        case 8: Console.Write("eighteen"); break;
+                        case 9: Console.Write("nineteen"); break;
+                    }
+
+                }
+
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("Good Bye!");
+    }
+}
+
